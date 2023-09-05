67a4393fd172381cbacf507e456261187baad970
Calvin Das <calvindas7@gmail.com>
2023-09-05 12:37:19 +0530

    google authenticatioin and mongo db connected

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Feed from "@components/Feed";
+import { connectToDB } from '@utils/database';
 
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      <Feed />
+    </section>
   )
 }
 
-export default page
+export default Home
