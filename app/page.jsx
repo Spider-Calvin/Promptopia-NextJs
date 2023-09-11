93dcdac52e52ee92f80e5ce3d904c1dc958c6ead
Calvin Das <calvindas7@gmail.com>
2023-09-11 11:58:10 +0530

    the console logs removed

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,6 @@ import Feed from "@components/Feed";
 import { connectToDB } from '@utils/database';
 
 const Home = () => {
-  console.log('process.env.NEXTAUTH_SECRET',process.env.NEXTAUTH_SECRET);
-  console.log('process.env.NEXTAUTH_SECRET',process.env.NEXTAUTH_URL);
-  console.log('process.env.NEXTAUTH_SECRET',process.env.NEXTAUTH_URL_INTERNAL);
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
